f7b1f5586a9db8cb50b772f6fee43f0d4ca11c40
Subham-102 <116659259+Subham-102@users.noreply.github.com>
2024-02-27 21:04:41 +0530

    Add files via upload

---
+++ array_Day2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int arr[5];
+    int max;
+    printf("Enter the elements of the array :- ");
+    for(int i=0;i<5;i++){
+        scanf("%d",&arr[i]);
+    }
+    max=arr[0];
+    for(int i=1;i<5;i++){
+        
+        if(max<arr[i]){
+            max=arr[i];
+        }
+    }
+    printf("Maximum element of the array is %d ",max);
+    return 0;
+}

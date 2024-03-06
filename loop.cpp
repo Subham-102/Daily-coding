cc9f4c539464c5da8e4c3c3e1859d14a7b1eaa98
Subham Shaw <subhamkrshawa21s@gmail.com>
2024-03-06 19:35:44 +0530

    Add files via upload

---
+++ loop.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num,range;
+    cout<<"Enter the number :- ";
+    cin>>num;
+    cout<<"Enter the range :- ";
+    cin>>range;
+    for(int i=1;i<=range;i++){
+        cout<<num <<"x"<< i<< "=" <<num*i<<endl;
+    }
+    return 0;
+}

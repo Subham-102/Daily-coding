152f2bd2404010f3429c19cdffcd435c72382ffc
Subham Shaw <subhamkrshawa21s@gmail.com>
2024-12-10 18:54:54 +0530

    Add files via upload

---
+++ s5.c
@@ -0,0 +1,10 @@
+//Write a C program to concatenate two strings.
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str1[]="sub";
+    char str2[]="vani";
+    strcat(str1,str2);
+    printf("%s",str1);
+    return 0; 
+}

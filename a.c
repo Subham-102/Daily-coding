35825258dc737d34d29ac530637241fe4e8b89b0
Subham Shaw <subhamkrshawa21s@gmail.com>
2025-01-31 13:56:47 +0530

    Add files via upload

---
+++ a.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    char str1[]="Hello";
+    char str2[]="Hello";
+    if(str1==str2){
+        printf("same");
+    }
+    else{
+        printf("not same");
+    }
+    return 0;
+}
